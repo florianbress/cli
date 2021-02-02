f6c4d6096fa360c6cc0be4d2fe9d491344c91535
jlorezz <florianbress@gmail.com>
2021-02-02 20:24:24 +0100

    base for the configuration / small restructuring

--- deps.ts
+++ deps.ts
@@ -1,2 +1,8 @@
 export { copy } from "https://deno.land/std@0.85.0/fs/mod.ts";
-export { Input } from "https://deno.land/x/cliffy@v0.17.0/prompt/mod.ts";
+export {
+  Checkbox,
+  Confirm,
+  Input,
+  Number,
+  prompt,
+} from "https://deno.land/x/cliffy/prompt/mod.ts";
